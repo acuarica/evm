ec4e9f3c36f856483ff103b2eaca2db7e02c815a
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-13 02:05:42 +0200

    Add `mocha` config

---
+++ .mocharc.js
@@ -0,0 +1,37 @@
+/**
+ * This configuration[1] is used when `mocha` is invoked directly
+ * from the command line.
+ *
+ * Note that this configuration does not affect tests
+ * when they are executed through `runTests`.
+ *
+ * [1] https://mochajs.org/#configuring-mocha-nodejs
+ */
+module.exports = {
+    /**
+     * From `mocha -h`
+     *
+     * ```txt
+     * --require, -r                    Require module      [array] [default: (none)]
+     * ```
+     */
+    require: [
+        // We use `ts-node/register`[1] to run our tests without compiling them.
+        //
+        // [1] https://typestrong.org/ts-node/docs/recipes/mocha/#mocha-7-and-newer.
+        'ts-node/register',
+    ],
+
+    /**
+     * From `mocha -h`
+     *
+     * ```txt
+     * spec  One or more files, directories, or globs to test
+     *                                                    [array] [default: ["test"]]
+     * ```
+     */
+    spec: [
+        // These are the same test files covered by the VS Code Host test runner.
+        'test/**/*.test.ts',
+    ],
+};
