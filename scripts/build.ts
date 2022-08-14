742a83a978d88c4528bcb67ec73fc5668597cf3e
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-14 17:38:19 +0200

    Fix build hashes script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,25 +1,21 @@
 import * as fs from 'fs';
-const web3 = require('web3');
+import { utils } from 'ethers';
 
 import * as functions from '../data/functions.json';
 import * as events from '../data/events.json';
 
-functions.sort((a, b) => a.localeCompare(b));
-events.sort((a, b) => a.localeCompare(b));
+type Hashes = { [hash: string]: string };
 
-const functionHashes: any = functions.reduce((map: any, functionName: any) => {
-    map[web3.utils.sha3(functionName).substring(2, 10)] = functionName;
-    return map;
-}, {});
+const reduce = (entries: string[], replacer: (value: string) => string) =>
+    entries.reduce((map: Hashes, entry: string) => {
+        map[replacer(utils.keccak256(utils.toUtf8Bytes(entry)))] = entry;
+        return map;
+    }, {});
 
-const eventHashes: any = events.reduce((map: any, eventName: any) => {
-    map[web3.utils.sha3(eventName).substring(2)] = eventName;
-    return map;
-}, {});
+const functionHashes = reduce(functions, s => s.substring(2, 10));
+const eventHashes = reduce(events, s => s.substring(2));
 
 fs.writeFileSync('./data/functionHashes.json', JSON.stringify(functionHashes, null, 4));
-fs.writeFileSync('./data/functions.json', JSON.stringify(functions, null, 4));
 fs.writeFileSync('./data/eventHashes.json', JSON.stringify(eventHashes, null, 4));
-fs.writeFileSync('./data/events.json', JSON.stringify(events, null, 4));
 
 console.log('Updated hashes successfully');
