7aaa727b28485be63270085ee65fdf93db66c8ab
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-16 17:52:15 +0200

    Refactor `dup` to avoid using `Opcode`

--- src/opcodes/dup.ts
+++ src/opcodes/dup.ts
@@ -1,7 +1,8 @@
 import { EVM } from '../evm';
 import { Opcode } from '../opcode';
 
-export default (opcode: Opcode, state: EVM): void => {
-    const duplicateLocation = parseInt(opcode.name.replace('DUP', ''), 10) - 1;
-    state.stack.dup(duplicateLocation);
+export default (position: number) => {
+    return (_opcode: Opcode, { stack }: EVM): void => {
+        stack.dup(position);
+    };
 };
