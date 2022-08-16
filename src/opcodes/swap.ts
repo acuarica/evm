ebf33b795aa362365272589e2f151f67be70ad8a
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-16 19:01:29 +0200

    Refactor `SWAP` to avoid using `Opcode`

--- src/opcodes/swap.ts
+++ src/opcodes/swap.ts
@@ -1,7 +1,8 @@
 import { EVM } from '../evm';
 import { Opcode } from '../opcode';
 
-export default (opcode: Opcode, state: EVM): void => {
-    const swapLocation = parseInt(opcode.name.replace('SWAP', ''), 10);
-    state.stack.swap(swapLocation);
+export default (position: number) => {
+    return (_opcode: Opcode, { stack }: EVM): void => {
+        stack.swap(position);
+    };
 };
---
+++ test/inst/swap.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import EVM from '../utils/evmtest';
+
+describe('SWAP', () => {
+    [...Array(16).keys()].forEach(function (size) {
+        it(`should swap #${size + 1} element on the stack`, () => {
+            const evm = new EVM('0x' + (0x90 + size).toString(16));
+            evm.stack.push(2n);
+
+            const ignored = [];
+            for (let i = 0; i < size; i++) {
+                ignored.push(1n);
+                evm.stack.push(1n);
+            }
+
+            evm.stack.push(3n);
+
+            evm.parse();
+
+            expect(evm.stack.elements).to.deep.equal([2n, ...ignored, 3n]);
+        });
+
+        it(`should throw when #${size + 2} element is not present on the stack`, () => {
+            const evm = new EVM('0x' + (0x90 + size).toString(16));
+
+            for (let i = 0; i <= size; i++) {
+                evm.stack.push(1n);
+            }
+
+            expect(() => evm.parse()).to.throw('Invalid swap operation');
+        });
+    });
+});
