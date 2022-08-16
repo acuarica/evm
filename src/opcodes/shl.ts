9ecc364c761cc4d141108b67b3f9d96b02d20a8b
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-16 13:31:58 +0200

    Fix shift instructions operand order

--- src/opcodes/shl.ts
+++ src/opcodes/shl.ts
@@ -3,30 +3,23 @@ import { Opcode } from '../opcode';
 import stringify from '../utils/stringify';
 
 export class SHL {
-    readonly name: string;
+    readonly name = 'SHL';
     readonly type?: string;
-    readonly wrapped: boolean;
-    readonly left: any;
-    readonly right: any;
+    readonly wrapped: boolean = true;
 
-    constructor(left: any, right: any) {
-        this.name = 'SHL';
-        this.wrapped = true;
-        this.left = left;
-        this.right = right;
-    }
+    constructor(readonly value: any, readonly shift: any) {}
 
     toString() {
-        return stringify(this.left) + ' << ' + stringify(this.right);
+        return stringify(this.value) + ' << ' + stringify(this.shift);
     }
 }
 
-export default (_opcode: Opcode, state: EVM): void => {
-    const left = state.stack.pop();
-    const right = state.stack.pop();
-    if (typeof left === 'bigint' && typeof right === 'bigint') {
-        state.stack.push(left << right);
-    } else {
-        state.stack.push(new SHL(left, right));
-    }
+export default (_opcode: Opcode, { stack }: EVM): void => {
+    const shift = stack.pop();
+    const value = stack.pop();
+    stack.push(
+        typeof value === 'bigint' && typeof shift === 'bigint'
+            ? value << shift
+            : new SHL(value, shift)
+    );
 };
--- src/opcodes/shr.ts
+++ src/opcodes/shr.ts
@@ -3,30 +3,24 @@ import { Opcode } from '../opcode';
 import stringify from '../utils/stringify';
 
 export class SHR {
-    readonly name: string;
+    readonly name = 'SHR';
     readonly type?: string;
-    readonly wrapped: boolean;
-    readonly left: any;
-    readonly right: any;
+    readonly wrapped = true;
 
-    constructor(left: any, right: any) {
-        this.name = 'SHR';
-        this.wrapped = true;
-        this.left = left;
-        this.right = right;
-    }
+    constructor(readonly value: any, readonly shift: any) {}
 
     toString() {
-        return stringify(this.left) + ' >>> ' + stringify(this.right);
+        return stringify(this.value) + ' >>> ' + stringify(this.shift);
     }
 }
 
-export default (_opcode: Opcode, state: EVM): void => {
-    const left = state.stack.pop();
-    const right = state.stack.pop();
-    if (typeof left === 'bigint' && typeof right === 'bigint') {
-        state.stack.push(left >> right);
-    } else {
-        state.stack.push(new SHR(left, right));
-    }
+export default (_opcode: Opcode, { stack }: EVM): void => {
+    const shift = stack.pop();
+    const value = stack.pop();
+
+    stack.push(
+        typeof value === 'bigint' && typeof shift === 'bigint'
+            ? value >> shift
+            : new SHR(value, shift)
+    );
 };
