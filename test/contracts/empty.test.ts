5a8440d459d50a7c3bb3aee0769cff8a6e7e2e5d
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-25 17:27:30 +0200

    Update hashes after changing compilation source

--- test/contracts/empty.test.ts
+++ test/contracts/empty.test.ts
@@ -4,9 +4,9 @@ import { SELFDESTRUCT } from '../../src/codes';
 import { compile, VERSIONS } from './utils/solc';
 
 const HASHES = {
-    '0.5.5': 'bzzr://e9e23a2fc4943df291ec2a003b44d656b4f1e45cad600af5a22979f8aefa8f7a',
-    '0.5.17': 'bzzr://7ad9f52cb195e236916f424f2b7e6f466b341ce9de1a732257c4a67848378ee4',
-    '0.8.16': 'ipfs://1220915bb2e4312c26653b8c0414c6e6d1e36938d91281c3e3dbb19092ba2f86dd2b',
+    '0.5.5': 'bzzr://21979a02bd63f5db5ee0ced275b81e8407ae1252d879b2143fe47da327f26dbd',
+    '0.5.17': 'bzzr://e7a2b3aa2410f08d6ab702e1d605cd779c106134ed242b57d0dbe67390d4a766',
+    '0.8.16': 'ipfs://1220b9c8a18d8f2f694ef2a30219acc4e9ddf79dd3eef10c395d968869a3480fd6cd',
 };
 
 const DECOMPILE = `revert();
