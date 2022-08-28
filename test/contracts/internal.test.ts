e6e53e10b867c4f38e64e65159badef3b35c10f7
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-28 06:36:37 +0200

    Test against multiple versions of `solc`

--- test/contracts/internal.test.ts
+++ test/contracts/internal.test.ts
@@ -1,31 +1,35 @@
 import { expect } from 'chai';
 import EVM from '../utils/evmtest';
-import { compile } from './utils/solc';
+import { compile, solcs, VERSIONS } from './utils/solc';
 
 describe('contracts::internal', () => {
-    describe('internal', () => {
-        let evm: EVM;
+    VERSIONS.forEach(version => {
+        describe(`using solc-v${solcs[version].version()}`, () => {
+            let evm: EVM;
 
-        before(() => {
-            const CONTRACT = `contract C {
-                mapping(address => uint256) private _values;
-                function _msgSender() internal view virtual returns (address) {
-                    return msg.sender;
-                }
-                function setInline(uint256 value) external {
-                    _values[msg.sender] = value + 3;
-                }
-                function setInternal(uint256 value) external {
-                    _values[_msgSender()] = value + 3;
-                }
-            }`;
-            evm = new EVM(compile('C', CONTRACT, '0.8.16'));
-        });
+            before(() => {
+                const CONTRACT = `contract C {
+                    mapping(address => uint256) private _values;
+                    function _msgSender() internal view returns (address) {
+                        return msg.sender;
+                    }
+                    function setInline(uint256 value) external {
+                        _values[msg.sender] = value + 3;
+                    }
+                    function setInternal(uint256 value) external {
+                        _values[_msgSender()] = value + 3;
+                    }
+                }`;
+                evm = new EVM(compile('C', CONTRACT, version));
+            });
 
-        it('should decompile functions', () => {
-            const text = evm.decompile();
-            expect(text, text).to.not.match(/return msg.sender;/);
-            expect(text, text).to.match(/storage\[keccak256\(msg.sender, 0\)\] = \(_arg0 \+ 3\)/);
+            it('should decompile bytecode', () => {
+                const text = evm.decompile();
+                expect(text, text).to.not.match(/return msg.sender;/);
+                expect(text, text).to.match(
+                    /storage\[keccak256\(msg.sender, 0\)\] = \(_arg0 \+ 3\)/
+                );
+            });
         });
     });
 });
--- test/contracts/modifiers.test.ts
+++ test/contracts/modifiers.test.ts
@@ -1,41 +1,43 @@
 import { expect } from 'chai';
 import EVM from '../utils/evmtest';
-import { compile } from './utils/solc';
+import { compile, solcs, VERSIONS } from './utils/solc';
 
 describe('contracts::modifiers', () => {
-    describe('modifiers', () => {
-        let evm: EVM;
+    VERSIONS.forEach(version => {
+        describe(`using solc-v${solcs[version].version()}`, () => {
+            let evm: EVM;
 
-        before(() => {
-            const CONTRACT = `contract C {
-                uint256 private _value;
-                address private _owner;
-                constructor () {
-                    address msgSender = _msgSender();
-                    _owner = msgSender;
-                }
-                modifier onlyOwner() {
-                    require(_owner == _msgSender(), "Ownable: caller is not the owner");
-                    _;
-                } 
-                function _msgSender() internal view virtual returns (address) {
-                    return msg.sender;
-                }
-                function setWithNoModifier(uint256 value) external {
-                    _value = value + 1;
-                }
-                function setWithModifier(uint256 value) external onlyOwner {
-                    _value = value + 3;
-                }
-            }`;
-            evm = new EVM(compile('C', CONTRACT, '0.8.16'));
-        });
+            before(() => {
+                const CONTRACT = `contract C {
+                    uint256 private _value;
+                    address private _owner;
+                    constructor() ${version === '0.8.16' ? '' : 'public '}{
+                        address msgSender = _msgSender();
+                        _owner = msgSender;
+                    }
+                    modifier onlyOwner() {
+                        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+                        _;
+                    } 
+                    function _msgSender() internal view returns (address) {
+                        return msg.sender;
+                    }
+                    function setWithNoModifier(uint256 value) external {
+                        _value = value + 1;
+                    }
+                    function setWithModifier(uint256 value) external onlyOwner {
+                        _value = value + 3;
+                    }
+                }`;
+                evm = new EVM(compile('C', CONTRACT, version));
+            });
 
-        it('should decompile bytecode', () => {
-            const text = evm.decompile();
-            expect(text, text).to.not.match(/return msg.sender;/);
-            expect(text, text).to.match(/if\(storage\[1\] == msg.sender\) \{$/m);
-            expect(text, text).to.match(/var1 = \(_arg0 \+ 3\);$/m);
+            it('should decompile bytecode', () => {
+                const text = evm.decompile();
+                expect(text, text).to.not.match(/return msg.sender;/);
+                expect(text, text).to.match(/if\(storage\[1\] == msg.sender\) \{$/m);
+                expect(text, text).to.match(/var1 = \(_arg0 \+ 3\);$/m);
+            });
         });
     });
 });
