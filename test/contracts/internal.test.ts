41948c7dba562c65b5b14d7d2724c0516b638cd8
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-28 05:24:44 +0200

    Add tests for `internal` functions

--- test/contracts/internal.test.ts
+++ test/contracts/internal.test.ts
@@ -4,61 +4,34 @@ import { compile } from './utils/solc';
 
 describe('contracts::internal', () => {
     describe('internal', () => {
-        const CONTRACT = `
-        abstract contract Context {
-            function _msgSender() internal view virtual returns (address) {
-                return msg.sender;
-            }
-        
-            function _msgData() internal view virtual returns (bytes calldata) {
-                this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-                return msg.data;
-            }
-        }
-
-        contract Ownable is Context {
-            address private _owner;
-            
-            constructor () {
-                address msgSender = _msgSender();
-                _owner = msgSender;
-            }
-        
-            function owner() public view returns (address) {
-                return _owner;
-            }
-        
-            modifier onlyOwner() {
-                require(_owner == _msgSender(), "Ownable: caller is not the owner");
-                _;
-            } 
-        }
-
-        contract Contract is Ownable {
-            mapping(address => uint256) private _values;
-            function set0(uint256 value) external {
-                _values[_msgSender()] = value + 0;
-            }
-            function set1(uint256 value) external onlyOwner {
-                _values[_msgSender()] = value + 1;
-            }
-            function set2(uint256 value) external {
-                _values[_msgSender()] = value + 2;
-            }
-            function set3(uint256 value) external onlyOwner {
-                _values[_msgSender()] = value + 3;
-            }
-        }`;
-
         let evm: EVM;
 
         before(() => {
-            evm = new EVM(compile('Contract', CONTRACT, '0.8.16'));
+            evm = new EVM(
+                compile(
+                    'C',
+                    `
+                contract C {
+                    mapping(address => uint256) private _values;
+                    function _msgSender() internal view virtual returns (address) {
+                        return msg.sender;
+                    }
+                    function setInline(uint256 value) external {
+                        _values[msg.sender] = value + 3;
+                    }
+                    function setInternal(uint256 value) external {
+                        _values[_msgSender()] = value + 3;
+                    }
+                }`,
+                    '0.8.16'
+                )
+            );
         });
 
-        it.skip('should decompile functions', () => {
-            console.log(evm.decompile());
-            expect(evm.decompile()).to.match(/function get\(\) public view payable/);
+        it('should decompile functions', () => {
+            const text = evm.decompile();
+            expect(text, text).to.not.match(/return msg.sender;/);
+            expect(text, text).to.match(/storage\[keccak256\(msg.sender, 0\)\] = \(_arg0 \+ 3\)/);
         });
     });
 });
