dd44d2bdf6d8dfca6fd709e43c1d85f971a933dc
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-28 16:39:11 +0200

    Remove unused functions
    There were first introduced in 033272e9cd881269cac72b114a6054b774a0b895.

--- src/hex.ts
+++ src/hex.ts
@@ -7,35 +7,6 @@ export function hex2a(hexx: any) {
     return str;
 }
 
-export function pad32(data: string) {
-    if (data.length >= 64) {
-        return data;
-    } else {
-        return '0'.repeat(64 - data.length) + data;
-    }
-}
-
-export function isHex(h: any) {
-    return /^[0-9a-f-]+$/.test(h);
-}
-
-export default (data: any) => {
-    if (isHex(data)) {
-        const pieces = data.match(/.{1,64}/g);
-        if (pieces.length === 3 && parseInt(pieces[0], 16) === 32) {
-            const stringLength = parseInt(pieces[1], 16) * 2;
-            const stringData = hex2a(pieces[2].substr(0, stringLength));
-            return '"' + stringData + '"';
-        } else if (!isNaN(parseInt(data, 16))) {
-            return parseInt(data, 16).toString();
-        } else {
-            return data;
-        }
-    } else {
-        return data;
-    }
-};
-
 /**
  *
  * @param buffer
