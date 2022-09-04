c2d3100d4597951fd5ad1b6a4b4f12073cec395c
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-09-04 05:22:02 +0200

    Embed mocha `spec` argument into `package.json`'s test scripts
    This avoids displaying `mocha` config files in coverage report.

--- .mocharc.js
+++ .mocharc.js
@@ -21,17 +21,4 @@ module.exports = {
         // [1] https://typestrong.org/ts-node/docs/recipes/mocha/#mocha-7-and-newer.
         'ts-node/register',
     ],
-
-    /**
-     * From `mocha -h`
-     *
-     * ```txt
-     * spec  One or more files, directories, or globs to test
-     *                                                    [array] [default: ["test"]]
-     * ```
-     *
-     * `test` tests perform the actual testing of the package's modules.
-     *
-     */
-    spec: ['test/**/*.test.ts'],
 };
