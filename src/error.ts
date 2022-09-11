bb807d741b99e4350ce69c3fc9ae6b6a784af39b
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-09-11 21:02:49 +0200

    Refine `assert` return type

--- src/error.ts
+++ src/error.ts
@@ -4,7 +4,7 @@ import { inspect } from 'util';
  *
  * @param condition
  */
-export function assert(condition: boolean, ...args: any[]) {
+export function assert(condition: boolean, ...args: any[]): asserts condition {
     if (!condition) {
         throw Error(`Assert failed: ${args.map(arg => inspect(arg, true, 6)).join(', ')}`);
     }
