28049a2072d6babb7954268041e03a60c72c2b0a
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-09-11 21:03:04 +0200

    Remove unused function `check`

--- src/error.ts
+++ src/error.ts
@@ -9,9 +9,3 @@ export function assert(condition: boolean, ...args: any[]): asserts condition {
         throw Error(`Assert failed: ${args.map(arg => inspect(arg, true, 6)).join(', ')}`);
     }
 }
-
-export function check(condition: boolean, message: string) {
-    if (!condition) {
-        throw new Error('check' + message);
-    }
-}
