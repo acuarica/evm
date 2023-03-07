940fc80bdb0359ef685296bb7625a1023e8500bd
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-07 16:44:38 +0100

    Merge `Stack` into `state` module

--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,77 @@
-import { Stack } from './stack';
 import { Expr, Stmt } from './ast';
+
+/**
+ *
+ */
+export class Stack<in out T> {
+    readonly values: T[] = [];
+
+    /**
+     *
+     */
+    get length() {
+        return this.values.length;
+    }
+
+    /**
+     *
+     * @param elem
+     */
+    push(elem: T): void | never {
+        if (this.values.length >= 1024) {
+            throw new Stack.Error('Stack too deep');
+        }
+
+        this.values.unshift(elem);
+    }
+
+    pop(): T | never {
+        if (this.values.length === 0) {
+            throw new Stack.Error('POP with empty stack');
+        }
+
+        // The non-null assertion operator `!` is needed here because the
+        // guard `length === 0` does not track array's emptiness.
+        // See https://github.com/microsoft/TypeScript/issues/30406.
+        return this.values.shift()!;
+    }
+
+    dup(position: number): void | never {
+        if (position < 0 || position > 15) {
+            throw new Stack.Error('Unsupported position for duplication operation');
+        } else if (!(position in this.values)) {
+            throw new Stack.Error('Invalid duplication operation, position was not found');
+        }
+
+        this.push(this.values[position]);
+    }
+
+    swap(secondPosition: number): void | never {
+        if (secondPosition < 1 || secondPosition > 16) {
+            throw new Stack.Error('Unsupported position for swap operation');
+        } else if (!(secondPosition in this.values)) {
+            throw new Stack.Error('Invalid swap operation, position was not found');
+        }
+
+        const firstValue = this.values[0];
+        const secondValue = this.values[secondPosition];
+        this.values[0] = secondValue;
+        this.values[secondPosition] = firstValue;
+    }
+
+    clone(): Stack<T> {
+        const stack = new Stack<T>();
+        stack.values.push(...this.values);
+        return stack;
+    }
+
+    static Error = class extends Error {
+        constructor(message: string) {
+            super(message);
+        }
+        override name = 'Stack.Error';
+    };
+}
 
 /**
  *
---
+++ test/state.test.ts
@@ -0,0 +1,99 @@
+import { expect } from 'chai';
+import { Stack, State } from '../src';
+
+describe('state', () => {
+    describe('Stack', () => {
+        it('should create an instance with an empty stack', () => {
+            const stack = new Stack<never>();
+            expect(stack.values).to.deep.equal([]);
+        });
+
+        it('should push successfully', () => {
+            const stack = new Stack<string>();
+            stack.push('1');
+            expect(stack.values).to.deep.equal(['1']);
+            stack.push('0');
+            expect(stack.values).to.deep.equal(['0', '1']);
+        });
+
+        it('should pop successfully', () => {
+            const stack = new Stack<number>();
+            stack.push(1);
+            stack.push(2);
+            expect(stack.values).to.deep.equal([2, 1]);
+            expect(stack.pop()).to.deep.equal(2);
+            expect(stack.values).to.deep.equal([1]);
+            expect(stack.pop()).to.deep.equal(1);
+            expect(stack.values).to.deep.equal([]);
+            expect(() => stack.pop()).to.throw(Error, 'POP with empty stack');
+        });
+
+        it('should duplicate successfully', () => {
+            const stack = new Stack<string>();
+            stack.push('x');
+            stack.push('y');
+            stack.push('z');
+            expect(stack.values).to.deep.equal(['z', 'y', 'x']);
+            stack.dup(0);
+            expect(stack.values).to.deep.equal(['z', 'z', 'y', 'x']);
+            stack.dup(3);
+            expect(stack.values).to.deep.equal(['x', 'z', 'z', 'y', 'x']);
+            expect(() => stack.dup(-1)).to.throw(
+                Error,
+                'Unsupported position for duplication operation'
+            );
+            expect(() => stack.dup(16)).to.throw(
+                Error,
+                'Unsupported position for duplication operation'
+            );
+            expect(() => stack.dup(5)).to.throw(
+                Error,
+                'Invalid duplication operation, position was not found'
+            );
+        });
+
+        it('should swap successfully', () => {
+            const stack = new Stack<string>();
+            stack.push('a');
+            stack.push('b');
+            stack.push('c');
+            expect(stack.values).to.deep.equal(['c', 'b', 'a']);
+            stack.swap(1);
+            expect(stack.values).to.deep.equal(['b', 'c', 'a']);
+            stack.swap(2);
+            expect(stack.values).to.deep.equal(['a', 'c', 'b']);
+            expect(() => stack.swap(0)).to.throw(Error, 'Unsupported position for swap operation');
+            expect(() => stack.swap(17)).to.throw(Error, 'Unsupported position for swap operation');
+            expect(() => stack.swap(3)).to.throw(
+                Error,
+                'Invalid swap operation, position was not found'
+            );
+        });
+
+        it('should clone successfully', () => {
+            const stack1 = new Stack<number>();
+            stack1.push(3);
+            expect(stack1.values).to.deep.equal([3]);
+            const stack2 = stack1.clone();
+            expect(stack2.values).to.deep.equal([3]);
+            stack1.pop();
+            expect(stack1.values).to.deep.equal([]);
+            expect(stack2.values).to.deep.equal([3]);
+        });
+    });
+
+    describe('State', () => {
+        it('should clone a state without aliasing with its source', () => {
+            const state = new State<never, number>();
+            expect(state.memory).to.have.be.empty;
+
+            state.memory[0] = 1;
+            const clone = state.clone();
+
+            state.memory[1] = 2;
+
+            expect(state.memory).to.have.keys([0, 1]);
+            expect(clone.memory).to.have.keys([0]);
+        });
+    });
+});
