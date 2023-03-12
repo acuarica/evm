f7087291b197708bdd781060e74f1a2868f97180
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-12 22:56:45 +0100

    Add more EVMs

---
+++ src/evm/def.ts
@@ -0,0 +1,80 @@
+import type { Add, Div, Exp, Mod, Mul, Sub } from './math';
+import type { DataCopy, Symbol0, Symbol1 } from './sym';
+
+/**
+ *
+ */
+export type Expr =
+    | Val
+    | Add
+    | Mul
+    | Sub
+    | Div
+    | Mod
+    | Exp
+    // | Lt | Gt | Eq | IsZero | And | Or | Xor | Not | Byte | Shl | Shr | Sar
+    // | Sig
+    // | CallValue | CallDataLoad | CallDataSize
+    | Symbol0
+    | Symbol1
+    | DataCopy;
+
+export function Tag<N extends string>(tag: N, prec: number) {
+    abstract class Tag {
+        readonly tag: N = tag;
+        static readonly prec = prec;
+        isVal(): this is Val {
+            return this.tag === 'Val';
+        }
+        isZero(): this is Val {
+            return this.isVal() && this.val === 0n;
+        }
+        isJumpDest() {
+            return this.isVal() && this.jumpDest !== null;
+        }
+
+        /**
+         * hghghg
+         */
+        abstract eval(): Expr;
+
+        /**
+         *
+         */
+        abstract str(): string;
+
+        _str(prec: any): string {
+            const text = this.str();
+            return Tag.prec < prec ? `(${text})` : text;
+        }
+    }
+
+    return Tag;
+}
+
+export function Bin<N extends string>(tag: N, op: string, prec: number) {
+    abstract class Bin extends Tag(tag, prec) {
+        constructor(readonly left: Expr, readonly right: Expr) {
+            super();
+        }
+
+        str() {
+            return `${this.left._str(prec)} ${op} ${this.right._str(prec)}`;
+        }
+    }
+
+    return Bin;
+}
+
+export class Val extends Tag('Val', 16) {
+    jumpDest: number | null = null;
+    constructor(readonly val: bigint) {
+        super();
+    }
+    eval(): Expr {
+        return this;
+    }
+    str(): string {
+        return `${this.isJumpDest() ? '[J]' : ''}${this.val.toString(16)}`;
+    }
+}
---
+++ src/evm/math.ts
@@ -0,0 +1,141 @@
+import type { Stack } from '../state';
+import { Bin, type Expr, Val } from './def';
+
+export class Add extends Bin('Add', '+', 11) {
+    eval(): Expr {
+        const left = this.left.eval();
+        const right = this.right.eval();
+        return left.isVal() && right.isVal()
+            ? new Val(left.val + right.val)
+            : left.isZero()
+            ? right
+            : right.isZero()
+            ? left
+            : new Add(left, right);
+    }
+}
+
+export class Mul extends Bin('Mul', '*', 12) {
+    eval(): Expr {
+        const lhs = this.left.eval();
+        const rhs = this.right.eval();
+        return lhs.isVal() && rhs.isVal()
+            ? new Val(lhs.val * rhs.val)
+            : lhs.isZero() || rhs.isZero()
+            ? new Val(0n)
+            : new Mul(lhs, rhs);
+    }
+}
+
+export class Sub extends Bin('Sub', '-', Add.prec) {
+    eval(): Expr {
+        const left = this.left.eval();
+        const right = this.right.eval();
+        return left.isVal() && right.isVal()
+            ? new Val(left.val - right.val)
+            : right.isZero()
+            ? left
+            : new Sub(left, right);
+    }
+}
+
+export class Div extends Bin('Div', '/', Mul.prec) {
+    eval(): Expr {
+        const left = this.left.eval();
+        const right = this.right.eval();
+        return left.isVal() && right.isVal()
+            ? right.val === 0n
+                ? new Div(left, right)
+                : new Val(left.val / right.val)
+            : right.isVal() && right.val === 1n
+            ? left
+            : new Div(left, right);
+    }
+}
+
+export class Mod extends Bin('Mod', '%', Mul.prec) {
+    eval(): Expr {
+        const lhs = this.left.eval();
+        const rhs = this.right.eval();
+        return lhs.isVal() && rhs.isVal() ? new Val(lhs.val % rhs.val) : new Mod(lhs, rhs);
+    }
+}
+
+export class Exp extends Bin('Exp', '**', 14) {
+    eval(): Expr {
+        const left = this.left.eval();
+        const right = this.right.eval();
+        return left.isVal() && right.isVal() && right.val >= 0
+            ? new Val(left.val ** right.val)
+            : new Exp(left, right);
+    }
+}
+
+export const MATH = {
+    ADD: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        stack.push(new Add(left, right));
+    },
+
+    MUL: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        stack.push(new Mul(left, right));
+    },
+
+    SUB: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        stack.push(new Sub(left, right));
+    },
+
+    DIV: div,
+    SDIV: div,
+    MOD: mod,
+    SMOD: mod,
+
+    ADDMOD: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        const mod = stack.pop();
+        stack.push(
+            left.isVal() && right.isVal() && mod.isVal()
+                ? new Val((left.val + right.val) % mod.val)
+                : left.isVal() && right.isVal()
+                ? new Mod(new Val(left.val + right.val), mod)
+                : new Mod(new Add(left, right), mod)
+        );
+    },
+
+    MULMOD: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        const mod = stack.pop();
+        stack.push(
+            left.isVal() && right.isVal() && mod.isVal()
+                ? new Val((left.val * right.val) % mod.val)
+                : left.isVal() && right.isVal()
+                ? new Mod(new Val(left.val * right.val), mod)
+                : new Mod(new Mul(left, right), mod)
+        );
+    },
+
+    EXP: (stack: Stack<Expr>): void => {
+        const left = stack.pop();
+        const right = stack.pop();
+        stack.push(new Exp(left, right));
+    },
+};
+
+function div(stack: Stack<Expr>): void {
+    const left = stack.pop();
+    const right = stack.pop();
+    stack.push(new Div(left, right));
+}
+
+function mod(stack: Stack<Expr>): void {
+    const left = stack.pop();
+    const right = stack.pop();
+    stack.push(new Mod(left, right));
+}
