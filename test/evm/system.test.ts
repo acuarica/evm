dda6bd441d8cb8464ec0e01f84b50dbb7a546b14
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-13 12:16:32 +0100

    Add test for `STOP`

--- test/evm/system.test.ts
+++ test/evm/system.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import type { Expr, Stmt } from '../../src/evm/ast';
-import { SYSTEM } from '../../src/evm/system';
+import { Stop, SYSTEM } from '../../src/evm/system';
 import { State } from '../../src/state';
 
 describe('evm::system', () => {
@@ -8,5 +8,7 @@ describe('evm::system', () => {
         const state = new State<Stmt, Expr>();
         SYSTEM.STOP(state);
         expect(state.halted).to.be.true;
+        expect(state.stmts).to.be.deep.equal([new Stop()]);
+        expect(`${state.stmts[0]}`).to.be.equal('return;');
     });
 });
