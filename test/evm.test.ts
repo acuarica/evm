c47331feb3a1af0dc802ef920d473e15ba39ca8d
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-13 03:21:35 +0100

    Remove pointless test

--- test/evm.test.ts
+++ test/evm.test.ts
@@ -2,11 +2,6 @@ import { expect } from 'chai';
 import EVM from './utils/evmtest';
 
 describe('evm', () => {
-    it('should create an instance', () => {
-        const evm = new EVM('0xfd');
-        expect(evm).to.be.an.instanceof(EVM);
-    });
-
     it('should create an instance with `INVALID` opcodes', () => {
         const evm = new EVM('0x0c0d0e0ffc');
         expect(evm).to.be.an.instanceof(EVM);
