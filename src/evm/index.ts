722456017a9bfddce4821e06d2e1480825811201
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-13 10:33:09 +0100

    Add `EVM` exec

---
+++ src/evm/index.ts
@@ -0,0 +1,158 @@
+import { formatOpcode, type Opcode } from '../opcode';
+import type { Stack, State as TState } from '../state';
+import type { Expr, Stmt } from './ast';
+
+import { PUSHES, STACK } from './stack';
+import { MATH } from './math';
+import { LOGIC } from './logic';
+import { ENV } from './env';
+import { SYM as SYMBOLS } from './sym';
+import { MEMORY } from './memory';
+import { INVALID, PC, SYSTEM } from './system';
+import { LOGS, type Events } from './log';
+
+// import { STORAGE } from './storage';
+
+import { assert } from '../error';
+
+type State = TState<Stmt, Expr>;
+
+function make<F, T extends { [mnemonic: string]: F }>(
+    table: T,
+    adapter: (fn: F) => (opcode: Opcode, state: State) => void
+) {
+    return Object.fromEntries(
+        Object.entries(table).map(([mnemonic, fn]) => [mnemonic, adapter(fn)])
+    ) as { [mnemonic in keyof T]: (opcode: Opcode, state: State) => void };
+}
+
+function makeStack<T extends { [mnemonic: string]: (stack: Stack<Expr>) => void }>(table: T) {
+    return make(table, (fn: (stack: Stack<Expr>) => void) => (_opcode, state) => fn(state.stack));
+}
+
+function makeState<T extends { [mnemonic: string]: (state: State) => void }>(table: T) {
+    return make(table, (fn: (state: State) => void) => (_opcode, state) => fn(state));
+}
+
+const TABLE = {
+    ...makeStack(MATH),
+    ...makeStack(LOGIC),
+    ...makeStack(ENV),
+    ...makeState(SYMBOLS),
+    ...makeState(MEMORY),
+    JUMPDEST: (_opcode: Opcode, _state: State) => {},
+    ...make(
+        PUSHES,
+        (fn: (pushData: Uint8Array, stack: Stack<Expr>) => void) => (opcode, state) =>
+            fn(opcode.pushData!, state.stack)
+    ),
+    ...makeStack(STACK<Expr>()),
+    ...makeState(SYSTEM),
+    PC,
+    INVALID,
+};
+
+export function EVM(opcodes: Opcode[], events: Events) {
+    const insts = {
+        ...TABLE,
+        // ...STORAGE(contract),
+        ...LOGS(events),
+    };
+
+    return {
+        opcodes,
+        exec(pc0: number, state: State) {
+            for (let pc = pc0; !state.halted && pc < opcodes.length; pc++) {
+                const opcode = opcodes[pc];
+
+                // block.opcodes.push(opcode);
+
+                if (opcode.mnemonic === 'JUMP') {
+                    const offset = state.stack.pop();
+                    const dest = getDest(offset);
+
+                    // const destBranch = makeBranch(branch, dest.pc, state);
+                    // state.stmts.push(new Jump(offset, destBranch));
+                    state.halted = true;
+                } else if (opcode.mnemonic === 'JUMPI') {
+                    const offset = state.stack.pop();
+                    const condition = state.stack.pop();
+
+                    const dest = getDest(offset);
+                    // const fallBranch = makeBranch(branch, opcode.pc + 1, state);
+                    // if (condition instanceof Sig) {
+                    //     this.functionBranches.push({
+                    //         hash: condition.hash,
+                    //         pc: dest.pc,
+                    //         state: state.clone(),
+                    //     });
+                    //     state.stmts.push(new SigCase(condition, fallBranch));
+                    // } else {
+                    //     const destBranch = makeBranch(branch, dest.pc, state);
+                    //     state.stmts.push(new Jumpi(condition, offset, fallBranch, destBranch));
+                    // }
+                    state.halted = true;
+                } else {
+                    try {
+                        // dispatch[opcode.mnemonic](opcode, state);
+                        insts[opcode.mnemonic as keyof typeof insts](opcode, state);
+                    } catch (err) {
+                        const message = (err as Error).message;
+                        throw new Error(
+                            `\`${message}\` at [${opcode.offset}] ${
+                                opcode.mnemonic
+                            } =| ${state.stack.values.join(' | ')}`
+                        );
+                    }
+                }
+
+                if (
+                    !state.halted &&
+                    pc < opcodes.length + 1 &&
+                    opcodes[pc + 1].mnemonic === 'JUMPDEST'
+                ) {
+                    // const fallBranch = makeBranch(branch, opcode.pc + 1, state);
+                    // state.stmts.push(new JumpDest(fallBranch));
+                    state.halted = true;
+                }
+            }
+
+            assert(state.halted);
+            // assert(!branch.state.halted);
+
+            /**
+             *
+             * @param offset
+             */
+            function getDest(offset: Expr): Opcode {
+                const offset2 = offset.eval();
+                if (!offset2.isVal()) {
+                    throw new Error('Expected numeric offset, found' + offset.toString());
+                }
+
+                const dest = opcodes.find(o => o.offset === Number(offset2.val));
+                if (!dest) {
+                    throw new Error(
+                        'Expected `JUMPDEST` in JUMP destination, but found is undefined'
+                    );
+                }
+
+                if (dest.mnemonic !== 'JUMPDEST') {
+                    throw new Error(
+                        'Expected `JUMPDEST` in JUMP destination, found' + formatOpcode(dest)
+                    );
+                }
+
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                (offset as any).isJumpDest = dest.pc;
+                return dest;
+            }
+
+            // function makeBranch(from: Branch, pc: number, state: State): Branch {
+            //     const to = from.to(pc, state.clone());
+            //     branches.unshift(to);
+            //     return to;
+            // }
+        },
+    };
+}
--- test/evm/sym.test.ts
+++ test/evm/sym.test.ts
@@ -1,14 +1,56 @@
+import assert = require('assert');
 import { expect } from 'chai';
-import type { Expr } from '../../src/evm/ast';
+import { EVM } from '../../src/evm';
+import type { Expr, Stmt } from '../../src/evm/ast';
 import { INFO, SYM, Symbol0 } from '../../src/evm/sym';
+import { stripMetadataHash } from '../../src/metadata';
+import { decode } from '../../src/opcode';
 import { State } from '../../src/state';
+import { compile } from '../utils/solc';
 
-describe('evm::sym::', () => {
+describe('evm::sym', () => {
     for (const [name, value] of Object.entries(INFO)) {
-        it(`should get \`${value}\` when executing \`${name}\``, () => {
-            const state = new State<never, Expr>();
-            SYM[name as keyof typeof INFO](state);
-            expect(state.stack.values).to.be.deep.equal([new Symbol0(value)]);
+        describe(`\`${value}\` from \`${name}\``, () => {
+            it('should get it programatically', () => {
+                const state = new State<never, Expr>();
+                SYM[name as keyof typeof INFO](state);
+                expect(state.stack.values).to.be.deep.equal([new Symbol0(value)]);
+            });
+
+            const sol = `contract C {
+                event Deposit(uint256);
+                fallback() external payable {
+                    emit Deposit(block.number);
+                }
+            }`;
+
+            let evm: ReturnType<typeof EVM>;
+
+            before(function () {
+                evm = EVM(
+                    decode(
+                        Buffer.from(
+                            stripMetadataHash(
+                                compile(sol, '0.7.6', { context: this }).deployedBytecode
+                            )[0],
+                            'hex'
+                        )
+                    ),
+                    {
+                        get: _topic => undefined,
+                        set: _topic => {},
+                    }
+                );
+            });
+
+            it('should get it from compiled code', () => {
+                const state = new State<Stmt, Expr>();
+                evm.exec(0, state);
+
+                const stmt = state.stmts[0];
+                assert(stmt.name === 'Log');
+                expect(stmt.args[0]).to.be.deep.equal(new Symbol0('block.number'));
+            });
         });
     }
 });
