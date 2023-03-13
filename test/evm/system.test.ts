7f07038cc2cf87a98ce72c4a17e2c75d2e45b9ae
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-13 06:33:14 +0100

    Remove old code and refactor `system` tests

---
+++ test/evm/system.test.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import type { Expr, Stmt } from '../../src/evm/ast';
+import { SYSTEM } from '../../src/evm/system';
+import { State } from '../../src/state';
+
+describe('evm::system::', () => {
+    it('should halt with `STOP`', () => {
+        const state = new State<Stmt, Expr>();
+        SYSTEM.STOP(state);
+        expect(state.halted).to.be.true;
+    });
+});
