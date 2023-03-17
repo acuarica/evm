594bf3dd084ea710f766359914b97878be10fcd9
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-17 23:45:57 +0100

    Merge `fetch` script into etherscan test to avoid running them separately

--- test/etherscan.smoke.ts
+++ test/etherscan.smoke.ts
@@ -1,20 +1,33 @@
 // import { expect } from 'chai';
-import { readFileSync } from 'fs';
+import chalk = require('chalk');
+import { providers } from 'ethers';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { EVM } from '../src/evm';
+
+const BASE_PATH = './data/smoke/';
 
 describe('etherscan', function () {
     this.timeout(5000);
 
-    const BASE_PATH = './data/smoke/';
+    /**
+     * Needs to be manually downloaded from
+     *
+     * https://etherscan.io/exportData?type=open-source-contract-codes
+     */
     const csv = readFileSync('data/export-verified-contractaddress-opensource-license.csv');
     csv.toString()
         .split('\n')
         .map(entry => entry.trimEnd().replace(/"/g, '').split(',') as [string, string, string])
-        .slice(0, 20)
-        .forEach(([, address, name]) => {
+        .slice(0, 50)
+        .forEach(([_tx, address, name]) => {
             const path = `${BASE_PATH}${name}-${address}.bytecode`;
 
-            it(`should decode & decompile ${path}`, () => {
+            it(`should decode & decompile ${name} ${address}`, async function () {
+                if (['InuNetwork', 'MetaShibarium'].includes(name)) {
+                    this.skip();
+                }
+
+                await fetchBytecode(name, address);
                 const bytecode = readFileSync(path, 'utf8');
                 const evm = EVM.from(bytecode);
                 if (
@@ -61,3 +74,39 @@ describe('etherscan', function () {
             });
         });
 });
+
+const provider = {
+    providers: [
+        new providers.JsonRpcProvider('https://api.mycryptoapi.com/eth'),
+        new providers.InfuraProvider(),
+        // new providers.AlchemyProvider(),
+        new providers.EtherscanProvider(),
+        new providers.CloudflareProvider(),
+    ],
+    current: 0,
+    getCode: async function (address: string) {
+        this.current = (this.current + 1) % this.providers.length;
+        const code = await this.providers[this.current].getCode(address);
+        await wait(2000);
+        return code;
+    },
+};
+
+function wait(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function fetchBytecode(name: string, address: string) {
+    const addr = chalk.blue;
+    const error = chalk.red;
+    const path = `${BASE_PATH}${name}-${address}.bytecode`;
+
+    if (!existsSync(path)) {
+        console.log(`Fetching code for ${name} at ${addr(address)} into ${BASE_PATH}`);
+        try {
+            const code = await provider.getCode(address);
+            writeFileSync(path, code);
+        } catch (err) {
+            console.log(error((err as { message: string }).message));
+        }
+    }
+}
