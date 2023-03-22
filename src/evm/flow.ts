7aebe139c536ef25d04558d504bbe5c223494873
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-22 22:34:27 +0100

    Add `errors` list into EVM instead of `throw`

--- src/evm/flow.ts
+++ src/evm/flow.ts
@@ -1,26 +1,17 @@
-import type { Stmt, Expr, IStmt, Val } from './expr';
+import type { Inst, Expr, IInst, Val } from './expr';
 import type { State } from '../state';
 import type { Sig } from './logic';
 import { type decode, formatOpcode, type Opcode } from '../opcode';
-import { Invalid } from './system';
-
-type Result<T, E> =
-    | { readonly tag: 'ok'; readonly val: T }
-    | { readonly tag: 'err'; readonly err: E };
-const Ok = <T>(val: T) => ({ tag: 'ok' as const, val });
-const Err = <E>(err: E) => ({ tag: 'err' as const, err });
-const isOk = <T, E>(result: Result<T, E>): result is ReturnType<typeof Ok<T>> =>
-    result.tag === 'ok';
 
 export class Branch {
-    constructor(readonly pc: number, readonly state: State<Stmt, Expr>) {}
+    constructor(readonly pc: number, readonly state: State<Inst, Expr>) {}
 
     get key() {
         return this.pc;
     }
 }
 
-export class Jump implements IStmt {
+export class Jump implements IInst {
     readonly name = 'Jump';
 
     constructor(readonly offset: Expr, readonly destBranch: Branch) {}
@@ -33,30 +24,37 @@ export class Jump implements IStmt {
     }
 }
 
-export class Jumpi implements IStmt {
+export class Jumpi implements IInst {
     readonly name = 'Jumpi';
 
+    readonly evalCond: Expr;
+
     constructor(
-        readonly condition: Expr,
+        readonly cond: Expr,
         readonly offset: Expr,
         readonly fallBranch: Branch,
         readonly destBranch: Branch
-    ) {}
+    ) {
+        this.evalCond = cond.eval();
+    }
 
     toString() {
-        return `when ${this.condition} goto ${this.destBranch.key} or fall ${this.fallBranch.key}`;
+        return `when ${this.cond} goto ${this.destBranch.key} or fall ${this.fallBranch.key}`;
     }
 
     next() {
-        return [this.destBranch, this.fallBranch];
+        return this.evalCond.isVal()
+            ? this.evalCond.val === 0n
+                ? [this.fallBranch]
+                : [this.destBranch]
+            : [this.destBranch, this.fallBranch];
     }
 }
 
-export class JumpDest implements IStmt {
+export class JumpDest implements IInst {
     readonly name = 'JumpDest';
     constructor(readonly fallBranch: Branch) {}
     toString() {
-        // return 'fall:' + this.fallBranch.key + ':' + this.fallBranch.path.join('->');
         return `fall: ${this.fallBranch.key}:`;
     }
 
@@ -65,7 +63,7 @@ export class JumpDest implements IStmt {
     }
 }
 
-export class SigCase implements IStmt {
+export class SigCase implements IInst {
     readonly name = 'SigCase';
     constructor(readonly condition: Sig, readonly offset: Expr, readonly fallBranch: Branch) {}
     toString() {
@@ -77,67 +75,47 @@ export class SigCase implements IStmt {
     }
 }
 
-export function makeBranch(pc: number, state: State<Stmt, Expr>) {
+export function makeBranch(pc: number, state: State<Inst, Expr>) {
     return new Branch(pc, state.clone());
 }
 
-export interface IEVMSelectorBranches {
+export interface ISelectorBranches {
     /**
      * store selectors starting point.
      */
-    readonly functionBranches: Map<string, { pc: number; state: State<Stmt, Expr> }>;
+    readonly functionBranches: Map<string, { pc: number; state: State<Inst, Expr> }>;
 }
 
 export function FLOW(
     { opcodes, jumpdests }: ReturnType<typeof decode>,
-    { functionBranches }: IEVMSelectorBranches
+    { functionBranches }: ISelectorBranches
 ) {
     return {
-        JUMP: (_opcode: Opcode, state: State<Stmt, Expr>): void => {
+        JUMP: (_opcode: Opcode, state: State<Inst, Expr>): void => {
             const offset = state.stack.pop();
-            const dest = getDest(offset);
-            if (isOk(dest)) {
-                const destBranch = makeBranch(dest.val, state);
-                state.halt(new Jump(offset, destBranch));
-            } else {
-                state.halt(new Invalid(dest.err));
-            }
+            const destpc = getDest(offset);
+            const destBranch = makeBranch(destpc, state);
+            state.halt(new Jump(offset, destBranch));
         },
 
-        JUMPI: (opcode: Opcode, state: State<Stmt, Expr>): void => {
+        JUMPI: (opcode: Opcode, state: State<Inst, Expr>): void => {
             const offset = state.stack.pop();
             const cond = state.stack.pop();
+            const destpc = getDest(offset);
 
-            const dest = getDest(offset);
-            if (isOk(dest)) {
-                const cond2 = cond.eval();
-                if (cond2.isVal()) {
-                    if (cond2.val === 0n) {
-                        const fallBranch = makeBranch(opcode.pc + 1, state);
-                        state.halt(new Jump(offset, fallBranch));
-                    } else {
-                        const destBranch = makeBranch(dest.val, state);
-                        state.halt(new Jump(offset, destBranch));
-                    }
-                    return;
-                }
+            const fallBranch = makeBranch(opcode.pc + 1, state);
 
-                const fallBranch = makeBranch(opcode.pc + 1, state);
-
-                let last: SigCase | Jumpi;
-                if (cond.tag === 'Sig') {
-                    functionBranches.set(cond.selector, {
-                        pc: dest.val,
-                        state: state.clone(),
-                    });
-                    last = new SigCase(cond, offset, fallBranch);
-                } else {
-                    last = new Jumpi(cond, offset, fallBranch, makeBranch(dest.val, state));
-                }
-                state.halt(last);
+            let last: SigCase | Jumpi;
+            if (cond.tag === 'Sig') {
+                functionBranches.set(cond.selector, {
+                    pc: destpc,
+                    state: state.clone(),
+                });
+                last = new SigCase(cond, offset, fallBranch);
             } else {
-                state.halt(new Invalid(dest.err));
+                last = new Jumpi(cond, offset, fallBranch, makeBranch(destpc, state));
             }
+            state.halt(last);
         },
     };
 
@@ -146,26 +124,21 @@ export function FLOW(
      * @param offset
      * @returns
      */
-    function getDest(offset: Expr): Result<number, string> {
+    function getDest(offset: Expr): number {
         const offset2 = offset.eval();
         if (!offset2.isVal()) {
-            return Err(`Expected numeric offset, found ${offset}`);
+            throw new Error(`Expected numeric offset, found ${offset}`);
         }
-        const dest = jumpdests[Number(offset2.val)];
-        if (dest !== undefined) {
-            (offset as Val).jumpDest = dest;
-            return Ok(dest);
+        const destpc = jumpdests[Number(offset2.val)];
+        if (destpc !== undefined) {
+            (offset as Val).jumpDest = destpc;
+            return destpc;
         } else {
             const dest = opcodes.find(o => o.offset === Number(offset2.val));
             if (!dest) {
-                return Err('Expected `JUMPDEST` in JUMP destination, but none was found');
+                throw new Error('Expected `JUMPDEST` in JUMP destination, but none was found');
             }
-            if (dest.mnemonic !== 'JUMPDEST') {
-                return Err('JUMP destination should be JUMPDEST but found' + formatOpcode(dest));
-            }
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            (offset as any).jumpDest = dest.pc;
-            return Ok(dest.pc);
+            throw new Error('JUMP destination should be JUMPDEST but found' + formatOpcode(dest));
         }
     }
 }
--- src/evm/index.ts
+++ src/evm/index.ts
@@ -1,8 +1,8 @@
-import { decode, type OPCODES, type Opcode, MNEMONICS } from '../opcode';
+import { decode, OPCODES, type Opcode, MNEMONICS } from '../opcode';
 import { type Stack, State as TState } from '../state';
 import { type Metadata, stripMetadataHash } from '../metadata';
 
-import type { Expr, IStmt, Stmt } from './expr';
+import type { Expr, IInst, Inst } from './expr';
 
 import { PUSHES, STACK } from './stack';
 import { MATH } from './math';
@@ -11,13 +11,13 @@ import { ENV } from './env';
 import { SYM as SYMBOLS } from './sym';
 import { MEMORY } from './memory';
 import { Invalid, INVALID, PC, SYSTEM } from './system';
-import { LOGS, type IEVMEvents } from './log';
-import { type IEVMStore, STORAGE } from './storage';
-import { Branch, FLOW, type IEVMSelectorBranches, JumpDest, makeBranch } from './flow';
+import { LOGS, type IEvents } from './log';
+import { type IStore, STORAGE } from './storage';
+import { Branch, FLOW, type ISelectorBranches, JumpDest, makeBranch } from './flow';
 
 import { assert } from '../error';
 
-type State = TState<Stmt, Expr>;
+type State = TState<Inst, Expr>;
 
 function make<F, T extends { [mnemonic: string]: F }>(
     table: T,
@@ -69,12 +69,12 @@ function fill<F>(insts: { [mnemonic in keyof typeof OPCODES]: F }) {
     );
 }
 
-export class EVM implements IEVMEvents, IEVMStore, IEVMSelectorBranches {
+export class EVM implements IEvents, IStore, ISelectorBranches {
     /**
      *
      */
     private readonly insts: {
-        [opcode in string]: (opcode: Opcode, state: TState<Stmt, Expr>) => void;
+        [opcode in string]: (opcode: Opcode, state: TState<Inst, Expr>) => void;
     };
 
     /**
@@ -82,15 +82,21 @@ export class EVM implements IEVMEvents, IEVMStore, IEVMSelectorBranches {
      */
     readonly chunks = new Map<number, { pcend: number; states: State[] }>();
 
-    readonly events: IEVMEvents['events'] = {};
-    readonly variables: IEVMStore['variables'] = {};
-    readonly mappings: IEVMStore['mappings'] = {};
-    readonly functionBranches: IEVMSelectorBranches['functionBranches'] = new Map<
+    /**
+     *
+     */
+    readonly errors: Invalid[] = [];
+
+    readonly events: IEvents['events'] = {};
+    readonly variables: IStore['variables'] = {};
+    readonly mappings: IStore['mappings'] = {};
+    readonly functionBranches: ISelectorBranches['functionBranches'] = new Map<
         string,
         { pc: number; state: State }
     >();
 
     readonly opcodes: ReturnType<typeof decode>['opcodes'];
+
     readonly jumpdests: ReturnType<typeof decode>['jumpdests'];
 
     constructor({ opcodes, jumpdests }: ReturnType<typeof decode>, readonly metadata?: Metadata) {
@@ -132,20 +138,20 @@ export class EVM implements IEVMEvents, IEVMStore, IEVMSelectorBranches {
             // See https://github.com/microsoft/TypeScript/issues/30406.
             const branch = branches.shift()!;
 
-            // const chunk = this.chunks.get(branch.pc);
-            // if (chunk !== undefined && chunk.states.length > 10) {
-            // continue;
-            // }
+            const chunk = this.chunks.get(branch.pc);
+            if (chunk !== undefined && chunk.states.length > 10) {
+                continue;
+            }
 
             this.exec(branch.pc, branch.state);
-            const last = branch.state.last! as IStmt;
+            const last = branch.state.last! as IInst;
             if (last.next) {
                 for (const b of last.next()) {
                     // const s = gc(b, this.chunks);
                     // if (s === undefined) {
                     branches.unshift(b);
                     // } else {
-                    // b.state = s;
+                    //     b.state = s;
                     // }
                 }
                 // branches.unshift(...last.next());
@@ -156,34 +162,28 @@ export class EVM implements IEVMEvents, IEVMStore, IEVMSelectorBranches {
     exec(pc0: number, state: State) {
         assert(!state.halted);
         let pc = pc0;
-        for (; !state.halted && pc < this.opcodes.length; pc++) {
+        const oplen = this.opcodes.length;
+        for (; !state.halted && pc < oplen; pc++) {
             const opcode = this.opcodes[pc];
             try {
-                // this.insts[opcode.mnemonic](opcode, state);
                 this.insts[opcode.opcode](opcode, state);
+                if (
+                    !state.halted &&
+                    pc < oplen + 1 &&
+                    this.opcodes[pc + 1].opcode === OPCODES.JUMPDEST
+                ) {
+                    const fallBranch = makeBranch(opcode.pc + 1, state);
+                    state.halt(new JumpDest(fallBranch));
+                }
             } catch (err) {
                 const message = (err as Error).message;
-                state.halt(
-                    new Invalid(
-                        `\`${message}\` at [${opcode.offset}] ${
-                            opcode.mnemonic
-                        } =| ${state.stack.values.slice(0, -1).join(' | ')}`
-                    )
-                );
-                throw new Error(
+                const inv = new Invalid(
                     `\`${message}\` at [${opcode.offset}] ${opcode.mnemonic} =| ${state.stack.values
-                        .slice(0, 20)
+                        .slice(0, -1)
                         .join(' | ')}`
                 );
-            }
-
-            if (
-                !state.halted &&
-                pc < this.opcodes.length + 1 &&
-                this.opcodes[pc + 1].mnemonic === 'JUMPDEST'
-            ) {
-                const fallBranch = makeBranch(opcode.pc + 1, state);
-                state.halt(new JumpDest(fallBranch));
+                state.halt(inv);
+                this.errors.push(inv);
             }
         }
 
@@ -215,7 +215,7 @@ function gc(b: Branch, chunks: EVM['chunks']) {
     return undefined;
 }
 
-function cmp({ stack: lhs }: TState<Stmt, Expr>, { stack: rhs }: TState<Stmt, Expr>) {
+function cmp({ stack: lhs }: TState<Inst, Expr>, { stack: rhs }: TState<Inst, Expr>) {
     if (lhs.values.length !== rhs.values.length) {
         return false;
     }
