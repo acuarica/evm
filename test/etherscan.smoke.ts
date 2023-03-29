46aabe14af4adfad81a68a2ea796cc23acba1797
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-03-29 13:32:25 +0200

    Restore some checks for etherscan tests

--- test/etherscan.smoke.ts
+++ test/etherscan.smoke.ts
@@ -1,8 +1,8 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
 import chalk = require('chalk');
 import { providers } from 'ethers';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { EVM } from '../src/evm';
+import { Contract } from '../src';
 
 const BASE_PATH = './data/smoke/';
 
@@ -11,7 +11,6 @@ const error = chalk.red;
 const provider = {
     providers: [
         new providers.InfuraProvider(),
-        // new providers.AlchemyProvider(),
         new providers.EtherscanProvider(),
         new providers.CloudflareProvider(),
         new providers.PocketProvider(),
@@ -39,21 +38,9 @@ describe('etherscan', function () {
         .trimEnd()
         .split('\n')
         .map(entry => entry.trimEnd().replace(/"/g, '').split(',') as [string, string, string])
-        // .slice(0, 4000)
+        // .slice(0, 500)
         .forEach(([_tx, address, name]) => {
             it(`should decode & decompile ${name} ${address}`, async function () {
-                if (
-                    [
-                        // 'InuNetwork',
-                        'MetaShibarium',
-                        'mirageAuction',
-                        'BSN',
-                        'ShibariumDollar',
-                    ].includes(name)
-                ) {
-                    // this.skip();
-                }
-
                 const path = `${BASE_PATH}${name}-${address}.bytecode`;
                 if (!existsSync(path)) {
                     this.timeout(10000);
@@ -70,52 +57,20 @@ describe('etherscan', function () {
                 }
 
                 const bytecode = readFileSync(path, 'utf8');
-                const evm = new EVM(bytecode);
-                if (
-                    [
-                        'Vyper_contract-0x5c22c615eefbaa896c6e34db8d1e9835ae215832',
-                        'Vyper_contract-0xA9b2F5ce3aAE7374a62313473a74C98baa7fa70E',
-                    ].includes(name + '-' + address)
-                ) {
-                    // expect(() => evm.contract).to.throw(
-                    //     '`Stack.Error: POP with empty stack` at [1158] MSTORE =| '
-                    // );
-                } else if (
-                    [
-                        'FairXYZWallets-0x033870acf44FaB6342EF1a114A6826D2F8D15B03',
-                        'VotingContract-0x60Fb0abAECc398F122c28dafc288D3EE6c2835D6',
-                    ].includes(name + '-' + address)
-                ) {
-                    // expect(() => evm.contract).to.throw(
-                    //     'TypeError: storeLocation.items is not iterable'
-                    // );
-                } else if (
-                    [
-                        'Snapshots-0xba31ab04a7fe99641e1e7884c21ecbe2692a3cdc',
-                        'RocketMinipoolFactory-0x54705f80D7C51Fcffd9C659ce3f3C9a7dCCf5788',
-                        'RocketNodeDistributorFactory-0xe228017f77B3E0785e794e4c0a8A6b935bB4037C',
-                    ].includes(name + '-' + address)
-                ) {
-                    // expect(() => evm.contract).to.throw('Error: memargs sizeclass');
-                } else {
-                    // const contract = evm.contract;
-                    // const externals = [
-                    //     ...Object.values(contract.functions)
-                    //         .filter(fn => fn.label !== fn.hash + '()')
-                    //         .map(fn => fn.label),
-                    //     ...Object.values(contract.variables)
-                    //         .filter(v => v.label !== undefined)
-                    //         .map(v => v.label! + '()'),
-                    // ];
-                    // expect(evm.getFunctions().sort()).to.include.members(externals.sort());
+                if (bytecode === '0x') {
+                    return;
                 }
 
-                evm.start();
-                // evm.run(0, new State());
-                // for (const [selector, branch] of evm.functionBranches) {
-                //     console.log(selector);
-                //     evm.run(branch.pc, branch.state);
-                // }
+                const contract = new Contract(bytecode).patch();
+                const externals = [
+                    ...Object.values(contract.functions).flatMap(fn =>
+                        fn.label !== undefined ? [fn.label] : []
+                    ),
+                    ...Object.values(contract.evm.variables).flatMap(v =>
+                        v.label !== undefined ? [v.label + '()'] : []
+                    ),
+                ];
+                expect(contract.getFunctions().sort()).to.include.members(externals.sort());
             });
         });
 });
