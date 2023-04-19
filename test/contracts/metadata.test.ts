064788637a5b96caf229bab1c54e2da0872b2343
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-04-19 15:33:23 +0200

    Improve contract metadata test

--- test/contracts/metadata.test.ts
+++ test/contracts/metadata.test.ts
@@ -1,22 +1,29 @@
-import * as crypto from 'crypto';
+// import * as crypto from 'crypto';
 import { expect } from 'chai';
 import { OPCODES } from '../../src/opcode';
 import { Contract } from '../../src';
 import { compile } from '../utils/solc';
 
+const includesFF = (hash: string) => Buffer.from(hash, 'hex').includes(OPCODES.SELFDESTRUCT);
+
 describe('contracts metadata', () => {
     let contract: Contract;
 
     before(() => {
-        const sol = `contract C { bytes32 constant data = "[rd]"; }`;
-        do {
-            const randomData = crypto.randomBytes(16).toString('hex');
-            contract = new Contract(compile(sol.replace('[rd]', randomData), '0.5.5').bytecode);
-        } while (contract.metadata && !contract.metadata.hash.includes('ff'));
+        const sol = 'contract C { bytes32 constant data = "605ed3230cfe24897f703fb05af4bdfd"; }';
+        // const solTemplate = `contract C { bytes32 constant data = "[rd]"; }`;
+        // let sol;
+        // do {
+        //     const randomData = crypto.randomBytes(16).toString('hex');
+        //     sol = solTemplate.replace('[rd]', randomData);
+        contract = new Contract(compile(sol, '0.5.5').bytecode);
+        // } while (contract.metadata && !includesFF(contract.metadata.hash));
+        // console.log(sol);
+        // console.log(contract.metadata!.hash);
     });
 
     it('should include false positive selfdestruct (`ff`) in metadata hash', () => {
-        expect(contract.metadata!.hash).to.include('ff');
+        expect(includesFF(contract.metadata!.hash)).to.be.true;
     });
 
     it('should not detect selfdestruct', () => {
