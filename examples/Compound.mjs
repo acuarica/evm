1986256d4077f11ae33370b0836424a0ef89977b
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-04-21 19:26:06 +0200

    Use ESM instead of TS for Compound example

---
+++ examples/Compound.mjs
@@ -0,0 +1,12 @@
+import { providers } from 'ethers';
+
+import { Contract } from '@acuarica/evm';
+import '@acuarica/evm/selector';
+
+const provider = new providers.EtherscanProvider();
+
+(async () => {
+    const bytecode = await provider.getCode('0x3FDA67f7583380E67ef93072294a7fAc882FD7E7');
+    const evm = new Contract(bytecode).patch();
+    console.log(evm.decompile());
+})();
