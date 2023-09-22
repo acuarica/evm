0e79563e40cbc09d6c00678d38ae15e9e38aa045
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-09-22 09:38:00 +0200

    Fix examples

--- examples/USDC.js
+++ examples/USDC.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 import { JsonRpcProvider } from 'ethers';
-import { Contract } from 'sevm-4byte';
+import { Contract } from 'sevm';
+import 'sevm-4byte';
 
 const provider = new JsonRpcProvider('https://api.avax-test.network/ext/bc/C/rpc');
 const bytecode = await provider.getCode('0x5425890298aed601595a70AB815c96711a31Bc65');
