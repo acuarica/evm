3dfa6181a501c0689302f7e2f85af255ccc37b14
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-09-22 12:18:09 +0200

    Update `patch` docs

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,12 @@ import {} from 'sevm';
 
 declare module 'sevm' {
     interface Contract {
+        /**
+         * It looks up in the signature and events database for matching hashes.
+         * 
+         * When a matching `function` or `event` is found,
+         * it patches the `function` or `event` with the corresponding signature. 
+         */
         patch(): this;
     }
 }
