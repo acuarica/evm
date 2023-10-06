fbe047b8cb10e23982e1a9013301a4c5f93b88cf
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-10-06 18:33:42 +0200

    Cache random bytes for metadata test

--- test/contracts/metadata.test.ts
+++ test/contracts/metadata.test.ts
@@ -1,33 +1,43 @@
-// import * as crypto from 'crypto';
+import { randomBytes } from 'crypto';
 import { expect } from 'chai';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
 
 import { Contract, OPCODES } from 'sevm';
 
 import { compile } from '../utils/solc';
 
-const includesFF = (hash: string) => Buffer.from(hash, 'hex').includes(OPCODES.SELFDESTRUCT);
-
 describe('contracts metadata', function () {
+    const includesFF = (hash: string) => Buffer.from(hash, 'hex').includes(OPCODES.SELFDESTRUCT);
+
     let contract: Contract;
 
     before(function () {
-        const sol = 'contract C { bytes32 constant data = "c4fbd955063067b19422684fc2e52142"; }';
-        // const solTemplate = `contract C { bytes32 constant data = "[rd]"; }`;
-        // let sol;
-        // do {
-        // const randomData = crypto.randomBytes(16).toString('hex');
-        // sol = solTemplate.replace('[rd]', randomData);
-        contract = new Contract(compile(sol, '0.5.5').bytecode);
-        // } while (contract.metadata && !includesFF(contract.metadata.hash));
-        // console.log(sol);
-        // console.log(contract.metadata!.hash);
+        const version = '0.5.5';
+        const randomDataPath = join('.solc', `${this.test!.parent!.title}.${version}.data`);
+        const solTemplate = `contract C { bytes32 constant data = "[randomData]"; }`;
+
+        try {
+            const randomData = readFileSync(randomDataPath, 'utf8');
+            const sol = solTemplate.replace('[randomData]', randomData);
+            contract = new Contract(compile(sol, version).bytecode);
+        } catch {
+            let sol: string, randomData: string;
+            do {
+                randomData = randomBytes(16).toString('hex');
+                sol = solTemplate.replace('[randomData]', randomData);
+                contract = new Contract(compile(sol, version).bytecode);
+            } while (contract.metadata && !includesFF(contract.metadata.hash));
+            writeFileSync(randomDataPath, randomData);
+            this.currentTest!.title += ` (random data ${randomData})`;
+        }
     });
 
-    it('should include false positive selfdestruct (`ff`) in metadata hash', function () {
+    it('should include selfdestruct `ff` in metadata hash', function () {
         expect(includesFF(contract.metadata!.hash)).to.be.true;
     });
 
-    it('should not detect selfdestruct', function () {
+    it('should not find `selfdestruct` reachable', function () {
         expect(contract.containsOpcode(OPCODES.SELFDESTRUCT)).to.be.false;
         expect(contract.containsOpcode('SELFDESTRUCT')).to.be.false;
     });
