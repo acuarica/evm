e18755455615a86ba93dedc95c9331ccc041638e
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-11-27 18:49:27 +0100

    Refactor `selfdestruct` test

--- test/contracts/selfdestruct.test.ts
+++ test/contracts/selfdestruct.test.ts
@@ -5,26 +5,18 @@ import { Contract, OPCODES } from 'sevm';
 import { contracts } from '../utils/solc';
 
 contracts('selfdestruct', (compile, fallback, version) => {
-    let contract: Contract;
-
     if (version === '0.8.16') return;
 
-    // eslint-disable-next-line mocha/no-top-level-hooks
-    before(function () {
+    it('should detect `SELFDESTRUCT` and `decompile` bytecode', function () {
         const src = `contract Test {
             ${fallback}() external payable {
                 selfdestruct(msg.sender);
             }
         }`;
-        contract = new Contract(compile(src, this, { enabled: true }).bytecode);
-    });
-
-    it('should detect selfdestruct', function () {
+        const contract = new Contract(compile(src, this, { enabled: true }).bytecode);
         expect(contract.evm.containsOpcode(OPCODES.SELFDESTRUCT)).to.be.true;
         expect(contract.evm.containsOpcode('SELFDESTRUCT')).to.be.true;
-    });
 
-    it('should `decompile` bytecode', function () {
         expect(contract.decompile()).to.be.equal('selfdestruct(msg.sender);\n');
     });
 });
