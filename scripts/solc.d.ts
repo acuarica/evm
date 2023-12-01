369b858cbd7d00815530e20a95e8df9ac0db898f
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-12-01 01:37:55 +0100

    :recycle: Unify `solc` types into ambient module

--- scripts/solc.d.ts
+++ scripts/solc.d.ts
@@ -1,8 +1,65 @@
 declare module 'solc' {
     /**
+     * https://docs.soliditylang.org/en/latest/using-the-compiler.html#input-description
+     */
+    interface SolcInputSettings {
+        /**
+         * Optional: Optimizer settings
+         */
+        optimizer?: {
+            /**
+             * Disabled by default.
+             * NOTE: enabled=false still leaves some optimizations on. See comments below.
+             * WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+             * it to false and would actually disable all the optimizations.
+             */
+            enabled?: boolean;
+            /**
+             * Switch optimizer components on or off in detail.
+             * The "enabled" switch above provides two defaults which can be tweaked here.
+             * If "details" is given, "enabled" can be omitted.
+             */
+            details?: {
+                /**
+                 * The peephole optimizer is always on if no details are given,
+                 * use details to switch it off.
+                 */
+                peephole?: boolean;
+                /**
+                 * The inliner is always off if no details are given,
+                 * use details to switch it on.
+                 */
+                inliner?: boolean;
+                /**
+                 * The unused jumpdest remover is always on if no details are given,
+                 * use details to switch it off.
+                 */
+                jumpdestRemover?: boolean;
+                /**
+                 * Sometimes re-orders literals in commutative operations.
+                 */
+                orderLiterals?: boolean;
+                /**
+                 * Removes duplicate code blocks.
+                 */
+                deduplicate?: boolean;
+                /**
+                 * Common subexpression elimination, this is the most complicated step but
+                 * can also provide the largest gain.
+                 */
+                cse?: boolean;
+                /**
+                 * Optimize representation of literal numbers and strings in code.
+                 */
+                constantOptimizer?: boolean;
+            };
+        };
+    }
+
+    /**
      * https://docs.soliditylang.org/en/latest/using-the-compiler.html#output-description
      */
-    export interface SolcOutput {
+    interface SolcOutput {
         /**
          * This contains the contract-level outputs.
          * It can be limited/filtered by the `outputSelection` settings.
@@ -18,7 +75,11 @@ declare module 'solc' {
                      * The Ethereum Contract ABI. If empty, it is represented as an empty array.
                      * See https://docs.soliditylang.org/en/develop/abi-spec.html
                      */
-                    abi: Member[];
+                    abi: ABI;
+                    /**
+                     * EVM-related outputs.
+                     */
+                    evm: { bytecode: Bytecode; deployedBytecode: Bytecode };
                 };
             };
         };
@@ -30,7 +91,7 @@ declare module 'solc' {
             /**
              * Mandatory ("error", "warning" or "info", but please note that this may be extended in the future)
              */
-            severity: string;
+            severity: 'error' | 'warning' | 'info';
             /**
              * Optional: the message formatted with source location
              */
@@ -38,10 +99,18 @@ declare module 'solc' {
         }[];
     }
 
+    type ABI = Member[];
+
+    type Bytecode = {
+        object: string;
+        opcodes: string;
+        sourceMap: string;
+    };
+
     /**
      * https://docs.soliditylang.org/en/latest/abi-spec.html#json
      */
-    export interface Member {
+    interface Member {
         type: 'function' | 'event' | 'constructor' | 'receive' | 'fallback';
 
         /**
@@ -63,5 +132,10 @@ declare module 'solc' {
         }[];
     }
 
-    export function compile(input: string): string;
+    function compile(input: string): string;
+}
+
+declare module 'solc/wrapper' {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+    export default function wrapper(soljson: unknown): typeof import('solc');
 }
--- test/utils/solc.ts
+++ test/utils/solc.ts
@@ -6,126 +6,12 @@ import * as path from 'path';
 import * as c from 'ansi-colors';
 import type { Runnable, Suite } from 'mocha';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const wrapper = require('solc/wrapper');
+import wrapper from 'solc/wrapper';
+import type { ABI, SolcInputSettings, SolcOutput } from 'solc';
 
 export const VERSIONS = ['0.5.5', '0.5.17', '0.6.12', '0.7.6', '0.8.16'] as const;
 
 type Version = (typeof VERSIONS)[number];
-
-type Bytecode = {
-    object: string;
-    opcodes: string;
-    sourceMap: string;
-};
-
-type ABI = {
-    type: 'function' | 'event';
-    name: string;
-    inputs: {
-        name: string;
-        type: string;
-    }[];
-}[];
-
-/**
- * https://docs.soliditylang.org/en/latest/using-the-compiler.html#input-description
- */
-interface SolcInputSettings {
-    /**
-     * Optional: Optimizer settings
-     */
-    optimizer?: {
-        /**
-         * Disabled by default.
-         * NOTE: enabled=false still leaves some optimizations on. See comments below.
-         * WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
-         * it to false and would actually disable all the optimizations.
-         */
-        enabled?: boolean;
-        /**
-         * Switch optimizer components on or off in detail.
-         * The "enabled" switch above provides two defaults which can be tweaked here.
-         * If "details" is given, "enabled" can be omitted.
-         */
-        details?: {
-            /**
-             * The peephole optimizer is always on if no details are given,
-             * use details to switch it off.
-             */
-            peephole?: boolean;
-            /**
-             * The inliner is always off if no details are given,
-             * use details to switch it on.
-             */
-            inliner?: boolean;
-            /**
-             * The unused jumpdest remover is always on if no details are given,
-             * use details to switch it off.
-             */
-            jumpdestRemover?: boolean;
-            /**
-             * Sometimes re-orders literals in commutative operations.
-             */
-            orderLiterals?: boolean;
-            /**
-             * Removes duplicate code blocks.
-             */
-            deduplicate?: boolean;
-            /**
-             * Common subexpression elimination, this is the most complicated step but
-             * can also provide the largest gain.
-             */
-            cse?: boolean;
-            /**
-             * Optimize representation of literal numbers and strings in code.
-             */
-            constantOptimizer?: boolean;
-        };
-    };
-}
-
-/**
- * https://docs.soliditylang.org/en/latest/using-the-compiler.html#output-description
- */
-interface SolcOutput {
-    /**
-     * This contains the contract-level outputs.
-     * It can be limited/filtered by the `outputSelection` settings.
-     */
-    contracts: {
-        'source.sol': {
-            /**
-             * If the language used has no contract names,
-             * this field should equal to an empty string.
-             */
-            [contractName: string]: {
-                /**
-                 * The Ethereum Contract ABI. If empty, it is represented as an empty array.
-                 * See https://docs.soliditylang.org/en/develop/abi-spec.html
-                 */
-                abi: ABI;
-                /**
-                 * EVM-related outputs.
-                 */
-                evm: { bytecode: Bytecode; deployedBytecode: Bytecode };
-            };
-        };
-    };
-    /**
-     * Optional: not present if no errors/warnings/infos were encountered
-     */
-    errors?: {
-        /**
-         * Mandatory ("error", "warning" or "info", but please note that this may be extended in the future)
-         */
-        severity: 'error' | 'warning' | 'info';
-        /**
-         * Optional: the message formatted with source location
-         */
-        formattedMessage: string;
-    }[];
-}
 
 const versionsLoaded = new Set<Version>();
 
@@ -199,9 +85,7 @@ export function compile(
 
     versionsLoaded.add(version);
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const solc = wrapper(require(path.resolve('.solc', `soljson-v${version}.js`))) as {
-        compile: (input: string) => string;
-    };
+    const solc = wrapper(require(path.resolve('.solc', `soljson-v${version}.js`)));
     const { errors, contracts } = JSON.parse(solc.compile(input)) as SolcOutput;
 
     if (errors !== undefined) {
