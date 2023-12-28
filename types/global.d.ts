b6140dd120640e371e6cec82ed38cdcfe81da03c
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-12-28 03:25:35 -0300

    Remove unused global declarations

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,14 +1,4 @@
 /**
- * Converts a union to an intersection type.
- * 
- * It works by distributing the union's type[^1] and reconstrucing it
- * by intersection the result parameter. 
- * 
- * [^1]: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html
- */
-type UnionToIntersection<U> = (U extends unknown ? (_: U) => void : never) extends (_: infer I) => void ? I : never;
-
-/**
  * https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/object.fromentries/index.d.ts
  */
 declare global {
@@ -20,10 +10,6 @@ declare global {
         ): [keyof T, T[keyof T]][];
 
         fromEntries<K, V>(entries: Iterable<readonly [K, V]>): { [k in K]: V };
-
-        setPrototypeOf<T>(o: unknown, proto: T): T;
-
-        assign<T, U extends unknown[]>(target: T, ...sources: U): T & UnionToIntersection<U[number]>;
     }
 }
 
