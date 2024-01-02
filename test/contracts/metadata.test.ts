c8ea5d942fa39c6e2c9cd1abc1ea02047a002ecd
Luis Mastrangelo <luismastrangelo@gmail.com>
2024-01-02 02:41:13 -0300

    Fix non-reacheable metadata test

--- test/contracts/metadata.test.ts
+++ test/contracts/metadata.test.ts
@@ -1,44 +1,41 @@
 import { randomBytes } from 'crypto';
 import { expect } from 'chai';
-import { readFileSync } from 'fs';
 import { join } from 'path';
+import * as fs from 'fs';
 
-import { EVM } from 'sevm';
+import { EVM, Shanghai } from 'sevm';
 
 import { compile } from '../utils/solc';
 
-describe.skip('contracts metadata', function () {
-    // const includesFF = (hash: string) => Buffer.from(hash, 'hex').includes(OPCODES.SELFDESTRUCT);
-    const includesFF = (hash: string) => hash.match(/^([0-9a-f]{2})*5b([0-9a-f]{2})*ff/) !== null;
+describe('contracts metadata', function () {
+    const SELFDESTRUCT = new Shanghai().opcodes().SELFDESTRUCT;
 
-    // let evm: EVM<{functionBranches:ISelectorBranches}>;
+    const includesFF = (hash: string) => Buffer.from(hash, 'hex').includes(SELFDESTRUCT);
 
-    it('should not find `selfdestruct` reachable', function () {
-        // it('should include selfdestruct `ff` in metadata hash', function () {
-        this.timeout(100000);
+    it('should not find `selfdestruct` reachable in metadata', function () {
         const version = '0.5.5';
         const randomDataPath = join('.solc', `${this.test!.parent!.title}.${version}.data`);
         const solTemplate = `contract Test { bytes32 constant data = "[randomData]"; }`;
 
-        let evm;
+        let randomData, evm;
         try {
-            const randomData = readFileSync(randomDataPath, 'utf8');
+            randomData = fs.readFileSync(randomDataPath, 'utf8');
             const src = solTemplate.replace('[randomData]', randomData);
             evm = EVM.new(compile(src, version, this).bytecode);
         } catch {
-            let src: string, randomData: string;
             do {
                 randomData = randomBytes(16).toString('hex');
-                src = solTemplate.replace('[randomData]', randomData);
+                const src = solTemplate.replace('[randomData]', randomData);
                 evm = EVM.new(compile(src, version, null).bytecode);
-                console.log(randomData, evm.metadata?.hash);
-            } while (!includesFF(evm.metadata!.hash) || !evm.containsOpcode('SELFDESTRUCT'));
-            // writeFileSync(randomDataPath, randomData);
-            this.currentTest!.title += ` (random data ${randomData})`;
+            } while (!includesFF(evm.metadata!.hash));
+            fs.writeFileSync(randomDataPath, randomData);
         }
 
+        this.test!.title += ` (data ${randomData} | hash ${evm.metadata?.hash})`;
+
         expect(includesFF(evm.metadata!.hash)).to.be.true;
-        // expect(evm.containsOpcode(OPCODES.SELFDESTRUCT)).to.be.false;
-        expect(evm.containsOpcode('SELFDESTRUCT')).to.be.false;
+
+        evm.start();
+        expect(evm.opcodes().filter(o => o.mnemonic === 'SELFDESTRUCT')).to.be.empty;
     });
 });
