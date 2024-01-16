55a9d2d4ce2337c10101a827167ac135f0f7351d
Luis Mastrangelo <luismastrangelo@gmail.com>
2024-01-16 00:38:34 -0300

    Refactor

--- test/ast.test.ts
+++ test/ast.test.ts
@@ -20,35 +20,35 @@ describe('::ast', function () {
     });
 
     describe('children', function () {
-        const F = <E>(expr: E, fn: (expr: E) => Expr[]) => [expr, fn(expr)] as const;
+        const t = <E>(expr: E, fn: (expr: E) => Expr[]) => [expr, fn(expr)] as const;
 
         [
-            F(new Val(1n), () => []),
-            F(new Add(new Val(1n), new Val(2n)), expr => [expr.left, expr.right]),
-            F(new Lt(new Val(1n), new Val(2n)), expr => [expr.left, expr.right]),
-            F(new IsZero(new Val(0n)), expr => [expr.value]),
-            F(new Not(new Val(0n)), expr => [expr.value]),
-            F(new Byte(new Val(0n), new Val(32n)), expr => [expr.pos, expr.data]),
-            F(new Shl(new Val(1n), new Val(32n)), expr => [expr.value, expr.shift]),
-            F(new Sig('1234'), () => [],),
+            t(new Val(1n), () => []),
+            t(new Add(new Val(1n), new Val(2n)), expr => [expr.left, expr.right]),
+            t(new Lt(new Val(1n), new Val(2n)), expr => [expr.left, expr.right]),
+            t(new IsZero(new Val(0n)), expr => [expr.value]),
+            t(new Not(new Val(0n)), expr => [expr.value]),
+            t(new Byte(new Val(0n), new Val(32n)), expr => [expr.pos, expr.data]),
+            t(new Shl(new Val(1n), new Val(32n)), expr => [expr.value, expr.shift]),
+            t(new Sig('1234'), () => [],),
 
-            F(new MLoad(new Val(32n)), expr => [expr.location]),
+            t(new MLoad(new Val(32n)), expr => [expr.location]),
 
-            F(Props['block.basefee'], () => []),
-            F(new Fn('BLOCKHASH', new Val(1234n)), expr => [expr.value]),
-            F(new DataCopy('calldatacopy', new Val(0n), new Val(32n)), expr => [expr.offset, expr.size]),
-            F(new DataCopy('extcodecopy', new Val(0n), new Val(32n), new Val(0x1234n)), expr => [expr.offset, expr.size, expr.address!]),
-            F(new CallValue(), () => []),
-            F(new CallDataLoad(new Val(24n)), expr => [expr.location]),
+            t(Props['block.basefee'], () => []),
+            t(new Fn('BLOCKHASH', new Val(1234n)), expr => [expr.value]),
+            t(new DataCopy('calldatacopy', new Val(0n), new Val(32n)), expr => [expr.offset, expr.size]),
+            t(new DataCopy('extcodecopy', new Val(0n), new Val(32n), new Val(0x1234n)), expr => [expr.offset, expr.size, expr.address!]),
+            t(new CallValue(), () => []),
+            t(new CallDataLoad(new Val(24n)), expr => [expr.location]),
 
-            F(new SLoad(new Val(32n), undefined), expr => [expr.slot]),
+            t(new SLoad(new Val(32n), undefined), expr => [expr.slot]),
 
-            F(new Sha3(new Val(32n), new Val(64n)), expr => [expr.offset, expr.size]),
-            F(new Sha3(new Val(32n), new Val(64n), [new Val(1n), new Val(2n)]), expr => [expr.offset, expr.size, expr.args![0], expr.args![1]]),
-            F(new Create(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.value, expr.offset, expr.size]),
-            F(new Create(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.value, expr.offset, expr.size]),
-            F(new ReturnData(new Val(32n), new Val(128n)), expr => [expr.retOffset, expr.retSize]),
-            F(new Create2(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.offset, expr.size, expr.value]),
+            t(new Sha3(new Val(32n), new Val(64n)), expr => [expr.offset, expr.size]),
+            t(new Sha3(new Val(32n), new Val(64n), [new Val(1n), new Val(2n)]), expr => [expr.offset, expr.size, expr.args![0], expr.args![1]]),
+            t(new Create(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.value, expr.offset, expr.size]),
+            t(new Create(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.value, expr.offset, expr.size]),
+            t(new ReturnData(new Val(32n), new Val(128n)), expr => [expr.retOffset, expr.retSize]),
+            t(new Create2(new Val(100n), new Val(32n), new Val(128n)), expr => [expr.offset, expr.size, expr.value]),
         ].forEach(([expr, children]) => {
             it(title(expr), function () {
                 expect(expr.children()).to.have.ordered.members(children);
