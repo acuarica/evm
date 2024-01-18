b6b8e1b4e326583080e4900af3edd94384e80edd
Luis Mastrangelo <luismastrangelo@gmail.com>
2024-01-18 01:41:30 -0300

    Test bin without chai exec defaults

--- test/bin.test.ts
+++ test/bin.test.ts
@@ -3,17 +3,15 @@ import chai, { expect } from 'chai';
 import './utils/snapshot';
 
 chai.use(chaiExec);
-chaiExec.defaults = {
-    command: 'bin/sevm.mjs',
-};
 
 describe('::bin', function () {
+    const sevm = 'bin/sevm.mjs';
 
     // Increase timeout to include support for Node 16 on Windows.
     this.timeout(5000);
 
     it('should exit with a zero code using `--help`', function () {
-        const cli = chaiExec('--help');
+        const cli = chaiExec(sevm, ['--help']);
 
         expect(cli).to.exit.with.code(0);
         expect(cli.stdout).to.matchSnapshot('out', this);
@@ -21,7 +19,7 @@ describe('::bin', function () {
     });
 
     it('should exit with non-zero code on unknown flag', function () {
-        const cli = chaiExec('-h');
+        const cli = chaiExec(sevm, ['-h']);
 
         expect(cli).to.exit.with.not.code(0);
         expect(cli).stdout.to.be.empty;
@@ -29,7 +27,7 @@ describe('::bin', function () {
     });
 
     it('should exit with non-zero code on unknown command', function () {
-        const cli = chaiExec('unknowncommand');
+        const cli = chaiExec(sevm, ['unknowncommand']);
 
         expect(cli).to.exit.with.not.code(0);
         expect(cli).stdout.to.be.empty;
@@ -41,7 +39,7 @@ describe('::bin', function () {
             "bytecode": "60806040525f80fdfea2646970667358221220213295e11660e0fa1851b6245c99f6d8ef0d1ad319b69a6483694b3a316c2dc564736f6c63430008150033",
             "abi": []
         }`;
-        const cli = chaiExec('metadata - --no-color', { input });
+        const cli = chaiExec(sevm, ['metadata', '-', '--no-color'], { input });
 
         expect(cli).to.exit.with.code(0);
         expect(cli.stdout).to.matchSnapshot('out', this);
@@ -49,7 +47,7 @@ describe('::bin', function () {
     });
 
     it('should run `dis` command and find non-reacheable chunk', function () {
-        const cli = chaiExec('dis - --no-color', { input: '0x6001600201600c56010203045b62fffefd5b00' });
+        const cli = chaiExec(sevm, ['dis', '-', '--no-color'], { input: '0x6001600201600c56010203045b62fffefd5b00' });
 
         expect(cli).to.exit.with.code(0);
         expect(cli.stdout).to.matchSnapshot('out', this);
