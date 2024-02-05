9d6a1ca6e8419b2168040296a74a35bb50435c20
Luis Mastrangelo <luismastrangelo@gmail.com>
2024-02-04 23:26:30 -0300

    Support to patch revert signatures

--- 4byte/index.js
+++ 4byte/index.js
@@ -10,12 +10,15 @@ const sevm = require('sevm');
  * @returns {{ [hash: string]: string[] }}
  */
 const mapHashes = hashes => Object.fromEntries(
-    Object.entries(hashes).map(([hash, matches]) => [hash, matches?.map(({ name }) => name) ?? [] ])
+    Object.entries(hashes).map(([hash, matches]) => [hash, matches?.map(({ name }) => name) ?? []])
 );
 
 sevm.Contract.prototype.patch = async function (/** @type {Partial<import('.').Lookup>} */lookup = {}) {
-    const functions = Object.keys(this.functions).map(selector => '0x' + selector).join(',');
-    const events = Object.keys(this.events).map(topic => '0x' + topic).join(',');
+    const keys = Object.keys;
+    /** @type {(selector: string) => string} */
+    const hex = selector => '0x' + selector;
+    const functions = [...keys(this.functions), ...keys(this.reverts)].map(hex).join(',');
+    const events = keys(this.events).map(hex).join(',');
 
     if (!lookup.function || !lookup.event) {
         const url = `https://api.openchain.xyz/signature-database/v1/lookup?function=${functions}&event=${events}`;
@@ -32,16 +35,20 @@ sevm.Contract.prototype.patch = async function (/** @type {Partial<import('.').L
 
     for (let [selector, fn] of Object.entries(this.functions)) {
         selector = '0x' + selector;
-        if (selector in lookup.function) {
+        if (selector in lookup.function)
             fn.label = lookup.function[selector][0];
-        }
     }
 
     for (let [topic, event] of Object.entries(this.events)) {
         topic = '0x' + topic;
-        if (topic in lookup.event) {
+        if (topic in lookup.event)
             event.sig = lookup.event[topic][0];
-        }
+    }
+
+    for (let [selector, decl] of Object.entries(this.reverts)) {
+        selector = '0x' + selector;
+        if (selector in lookup.function)
+            decl.sig = lookup.function[selector][0];
     }
 
     return this;
