0e7f953eb663ddf559c3022a185b9416e1a48687
Luis Mastrangelo <luismastrangelo@gmail.com>
2026-05-15 23:16:13 +0200

    more wip

--- types/global.d.ts
+++ types/global.d.ts
@@ -11,6 +11,15 @@ declare global {
 
         fromEntries<K, V>(entries: Iterable<readonly [K, V]>): { [k in K]: V };
     }
+
+    /**
+     * > _The constructor of a mixin class (if any) must have a single rest parameter of type `any[]` and
+     * > must use the spread operator to pass those parameters as arguments in a `super(...args)` call._
+     * 
+     * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#support-for-mix-in-classes
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    type Cons<T extends object> = new (...args: any[]) => T;
 }
 
 export { };
